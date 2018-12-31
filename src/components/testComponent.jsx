e4170fe12484006f75416f61b8c5b742b6d58479
ShyGuyPy <luke.vawter1@gmail.com>
2018-12-31 16:49:52 -0500

    built a basic component

---
+++ src/components/testComponent.jsx
@@ -0,0 +1,11 @@
+import React, {Component } from 'react';
+
+class TestComp extends Component {
+    //state={}
+    render(){
+        
+        return ( <h1>This is a React Component</h1>)
+    }
+}
+
+export default TestComp;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import TestComp from './components/testComponent';
+
+//import 'bootstrap/dist/css/bootstrap.css'
 
 const element = <h1>Babel compile this please</h1>
-ReactDOM.render(element, document.getElementById('root'))
+const test = <p>kiss this</p>
+const test2 = <p2>or this</p2>
+
+ReactDOM.render(<TestComp/>, document.getElementById('root'))
+//ReactDOM.render(element, document.getElementById('root'))
